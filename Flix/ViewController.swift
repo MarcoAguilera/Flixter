afe9923f5d40e6bafeb8434ba7434a8df45a2bfd
Marco Aguilera <marcoaguilera@Marcos-Mac.local>
2020-02-04 14:51:49 -0800

    First Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Marco Aguilera on 2/4/20.
+//  Copyright © 2020 Marco Aguilera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
